d9b7085550eba6838d2138b81f799400e021abd9
Bobby Jasuja <30449951+bjprogrammer@users.noreply.github.com>
2017-12-23 15:13:03 -0500

    Update config.php

--- PHP/udr/config.php
+++ PHP/udr/config.php
@@ -1,9 +1,9 @@
 <?php
-define("DB_HOST", "fdb14.biz.nf");
-define("DB_USER", "2104393_bj");          
-define("DB_PASSWORD", "Bobby1993#");      
+define("DB_HOST", "fb14.biz.nf");
+define("DB_USER", "");          
+define("DB_PASSWORD", "");      
 define("DB_DATABASE", "2104393_bj");      
-define("GOOGLE_API_KEY", "AIzaSyDMnunAT4i2zhsYCDILBfpG3dqlDlCMw58"); // Place your Google API Key
+define("GOOGLE_API_KEY", "YOUR API KEY"); // Place your Google API Key
 define("GOOGLE_API_URL","https://gcm-http.googleapis.com/gcm/send");
 define("PWD", "./");
 ?>
